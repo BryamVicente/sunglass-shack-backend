12ef99f977873bb95bafd2ebe7b013dae3a78eb4
ajakcyer <ajakcyer97@gmail.com>
2020-11-27 18:37:45 -0500

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,3 +18,41 @@ product1 = Product.create!(description: "just splendid", name:" Aviator Classic 
 cart1 = Cart.create!(user_id: ajak.id, history: false)
 
 cartP = CartProduct.create!(cart_id: cart1.id, product_id: product1.id, quantity: 1)
+
+Product.create!(description: "cool glasses", name: "Aviator Classics", image: "https://assets.ray-ban.com/is/image/RayBan/805289602057_shad_qt?$594$", price: 130, brand_id: brand1.id)
+
+Product.create!(description: "Verry cool", name: "Club Master Classics", image: "https://assets.ray-ban.com/is/image/RayBan/805289346913_shad_qt?$594$ ", price: 150, brand_id: brand1.id)
+
+Product.create!(description: "Super cool", name: "Erika Metal", image: "https://assets.ray-ban.com/is/image/RayBan/8053672587692_shad_qt?$594$", price: 160, brand_id: brand1.id)
+
+# Ray-Ban:
+# sunglasses 1: 
+# image: https://assets.ray-ban.com/is/image/RayBan/805289602057_shad_qt?$594$ 
+# name: Aviator Classic 
+# sunglasses 2: 
+# image: https://assets.ray-ban.com/is/image/RayBan/805289346913_shad_qt?$594$ 
+# name: Club Master Classic 
+# sunglasses 3: 
+# image: https://assets.ray-ban.com/is/image/RayBan/8053672587692_shad_qt?$594$ 
+# name: Erika Metal
+
+brand2 = Brand.create!(name: "Persol")
+
+Product.create!(description: "awesome", name: "Just Vibing", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuKrH43e5n-GmYPPe9PBHg7PyrL5ijy8Qy2NVV9idwiIyI0q5Ny8QepeGSaHTE9ANxA8Hzc_Q&usqp=CAc ", price: 180, brand_id: brand2.id)
+
+
+Product.create!(description: "cool", name: "Don't know", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCw8Ilwdt5cNFHSTqJRJCa83h_zM_kbUZB1nUd63K8C-RxbTZeaajHsY-o89-LcOxI9Ke4sA&usqp=CAc", price: 180, brand_id: brand2.id)
+
+
+Product.create!(description: "coniceol", name: "Fashion", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBYgOGUPmHl918OwT9P0KNrv2dM_DPA1v1BjE22ljK-OHSq_HJF_KjwN_KSOl_wWn8DoME5y4&usqp=CAc", price: 180, brand_id: brand2.id)
+
+# Persol
+# sunglasses 1: 
+# image: https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTuKrH43e5n-GmYPPe9PBHg7PyrL5ijy8Qy2NVV9idwiIyI0q5Ny8QepeGSaHTE9ANxA8Hzc_Q&usqp=CAc 
+# name: Just vibing 
+# sunglasses 2: 
+# image: https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCw8Ilwdt5cNFHSTqJRJCa83h_zM_kbUZB1nUd63K8C-RxbTZeaajHsY-o89-LcOxI9Ke4sA&usqp=CAc  
+# # name: don’t know 
+# sunglasses 3: 
+# image: https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBYgOGUPmHl918OwT9P0KNrv2dM_DPA1v1BjE22ljK-OHSq_HJF_KjwN_KSOl_wWn8DoME5y4&usqp=CAc 
+# name: it’s fashion!
