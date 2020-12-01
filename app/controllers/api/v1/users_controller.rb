f791608050f25ee8fe1a6a37c28648a39cf898b9
ajakcyer <ajakcyer97@gmail.com>
2020-12-01 18:14:45 -0500

    added user create action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::UsersController < ApplicationController
 
-    skip_before_action :authorized, only: [:index, :show, :create]
+    skip_before_action :authorized, only: [:show, :create]
 
     def index
         users = User.all
@@ -13,7 +13,14 @@ class Api::V1::UsersController < ApplicationController
     end
 
     def create
+        @user = User.create(user_params)
         byebug
+        if @user.valid?
+            token = encode_token({user_id: @user.id})
+            render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created 
+        else
+            render json: {error: 'failed to create user'}, status: :not_acceptable
+        end
     end
 
     def update
@@ -22,6 +29,12 @@ class Api::V1::UsersController < ApplicationController
 
     def profile
         render json: {user: UserSerializer.new(current_user)}, status: :accepted
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:first_name, :last_name, :username, :age, :email, :password, :address, :address2, :city, :state, :zipcode)
     end
 
     
